4914a4a1f0a95ae967ffe246c3e730d5bf6b9697
Paul Arcoleo <paularcoleo@gmail.com>
2018-07-03 15:00:18 -0400

    favicon

--- duopoet/views/poems_main.py
+++ duopoet/views/poems_main.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 
 from duopoet import app
 from duopoet.services import PoemService
@@ -9,3 +9,7 @@ ps = PoemService()
 def poems():
     poems = ps.get_n_most_recent(60)
     return render_template('poems_main.html', poems=poems)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', filename='img/duopoet.png')
